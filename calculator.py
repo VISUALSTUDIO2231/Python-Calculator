92faee07e15891d9c1e7df3134d40e27e23c1a49
VISUALSTUDIO2231 <100051110+VISUALSTUDIO2231@users.noreply.github.com>
2026-04-08 10:41:21 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+first_number = int(input("whats your first number "))
+sign = (input(" what sign "))
+second_number = int(input("whats yout second number "))
+
+if sign == "+":
+    print(first_number + second_number)
+
+elif sign == "-":
+    print(first_number - second_number)
+    
+elif sign == "*":
+    print(first_number * second_number)
